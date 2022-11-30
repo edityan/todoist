23e398fea646d2c05fb0234f2014cc22456f930b
roderikmogot <roderikmogot17@gmail.com>
2022-11-30 10:25:27 +0700

    update tw file config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // tailwind.config.js
 module.exports = {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-	],
+	content: ["./src/**/*.{html,js}"],
 	theme: {
 		extend: {},
 	},
