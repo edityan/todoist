c4686a578ea0730d7a64d8148ed7d2cc36c52009
roderikmogot <roderikmogot17@gmail.com>
2022-11-30 14:07:53 +0700

    added inline display for medium screens

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -88,13 +88,14 @@ const Background = () => {
 	return (
 		<div className="flex flex-col justify-center items-center gap-5">
 			<div className="text-4xl font-bold text-center">
-				Organize your <span className="block">work and life,</span> finally.
+				Organize your <span className="block md:inline">work and life,</span>
+				finally.
 			</div>
 			<div className="text-center text-xl">
 				Become focused, organized,
-				<span className="block">and calm with Todoist.</span>
+				<span className="block md:inline">and calm with Todoist.</span>
 				<span className="block">The world&apos;s #1 task manager</span>
-				<span className="block">and to-do list app.</span>
+				<span className="block md:hidden">and to-do list app.</span>
 			</div>
 			<button className="bg-[#2596be] py-2 px-4 rounded-md text-white">
 				<Link to="/login">Start for free</Link>
